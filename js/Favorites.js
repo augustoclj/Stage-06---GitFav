f1a743103ab49b237c24edb266f5798ba1b17210
Augusto Julietti <augusto.julietti@gmail.com>
2022-09-12 16:49:00 -0300

    Desenvolvido Javascript para controlar adição/ remoção de perfis de Github favoritos

---
+++ js/Favorites.js
@@ -0,0 +1,147 @@
+import { GithubUser } from "./GithubUser.js"
+
+export class Favorites{
+  constructor(root){
+    this.root = document.querySelector(root)
+    this.load()
+    this.onadd()
+  }
+
+
+  load(){
+    this.entries = JSON.parse(localStorage.getItem('@github-favorites:')) || []
+  }
+
+  save(){
+    localStorage.setItem('@github-favorites:', JSON.stringify(this.entries))
+  }
+
+  delete(user){
+    const filteredEntries = this.entries.filter(entry => entry.login !== user.login)
+    
+    this.entries = filteredEntries
+
+    this.update()
+    this.save()
+  }
+
+  async add(username){
+    try{
+
+      const userExists = this.entries.find(entry => entry.login === username)
+
+      if(userExists){
+        throw new Error('Usuário já cadastrado')
+      } 
+
+      const user = await GithubUser.search(username)
+
+      if(user.login === undefined){
+        throw new Error('Usuário não encontrado!')
+      }
+      this.entries = [user, ...this.entries]
+      this.update()
+      this.save()
+
+    } catch(error){
+      alert(error.message)
+    }
+
+  }
+}
+
+
+export class FavoritesView extends Favorites{
+  constructor(root){
+    super(root)
+
+    this.tbody = this.root.querySelector('table tbody');
+
+    this.update();
+  }
+  
+  onadd(){
+    const addButton = this.root.querySelector('#search_github button')
+    addButton.onclick = () => {
+      const {value} = this.root.querySelector('#search_github input')
+      this.add(value)
+    }
+  }
+
+  update(){
+    this.removeAllTr();
+
+    if(this.entries.length <= 0){
+
+      const row = this.createRowEmptyEntries()
+      this.tbody.append(row)
+
+    }else{
+      
+      this.entries.forEach(user => {
+      const row = this.createRow();
+      
+      row.querySelector('.user img').src = `https://github.com/${user.login}.png`
+      row.querySelector('.user img').alt = `Imagem de ${user.name}`
+      row.querySelector('.user a').href = `https://github.com/${user.login}`
+      row.querySelector('.user p').textContent = user.name
+      row.querySelector('.user span').textContent = user.login
+      row.querySelector('.repositories').textContent = user.public_repos
+      row.querySelector('.followers').textContent = user.followers
+      row.querySelector('.remove').onclick = () => {
+        const isOk = confirm('Tem certeza que deseja deletar essa linha ?')
+        if(isOk){
+          this.delete(user)
+        }
+      }
+      
+      this.tbody.append(row)
+    })
+
+    }
+    
+    
+  }
+
+  createRow(){
+
+    const tr = document.createElement('tr');
+
+    tr.innerHTML = `
+        <td class="user">
+          <img src="https://github.com/augustoclj.png" alt="Imagem de Augusto Julietti"/>
+          <a href="https://github.com/augustoclj" target="_blank">
+            <p>Augusto Julietti</p>
+            <span>/augustoclj</span>
+          </a>
+        </td>
+        <td class="repositories"> 10 </td>
+        <td class="followers"> 1000 </td>
+        <td> <button class="remove"> Remover </button> </td>
+    `;
+
+    return tr
+  }
+
+  createRowEmptyEntries(){
+    const tr = document.createElement('tr');
+
+    tr.innerHTML = `
+          <td class='empty_favorites'>
+            <img src="./images/Estrela.svg" alt="Imagem estrela, nenhum favorito ainda">
+            <p>Nenhum favorito ainda</p>
+          </td>
+    `
+    return tr
+  }
+
+  removeAllTr(){
+    
+  
+    this.tbody.querySelectorAll('tr')
+      .forEach((tr) => {
+        tr.remove()
+      })
+  }
+
+}
